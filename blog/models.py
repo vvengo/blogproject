2f20f9e359758fd53698b1a12cb59d04dda0d80c
vvengo <wbli321@foxmail.com>
2017-12-06 15:47:25 +0800

    修改on_delete错误

--- blog/models.py
+++ blog/models.py
@@ -23,9 +23,9 @@ class Post(models.Model):
     create_time = models.DateTimeField()
     modified_time = models.DateTimeField()
     excerpt = models.CharField(max_length=200, blank=True)
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
